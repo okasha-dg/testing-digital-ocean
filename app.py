2349df931e4353b812e398d67ef8cc968114d804
Okasha <okasha@designgurus.org>
2024-02-01 22:21:21 +0000

    latest

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     branch_name = os.getenv('BRANCH_NAME', 'main')
-    return f'Hello, World! This is the {branch_name} branch. And now we test new build'
+    return f'Hello, World! This is the {branch_name} branch. And now we test new build latest'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
